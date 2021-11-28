f8c404360700c2a22c6cc5ebf0fb38a2d8d00d7b
Reydel <Reydelgutierrez1999@gmail.com>
2021-11-28 11:29:42 -0500

    Fixing interfaces 11/28

--- Interfaces/feJS/frontend.js
+++ Interfaces/feJS/frontend.js
@@ -1,12 +1,27 @@
+var nameArray = [];
+
 function ForgotEFunction(){
 alert("Email Sent, Please make sure email is inserted before clicking");
 }
 
 function addNameToList(){
-  var nameInputed = document.getElementById("addNameForm").placeholder;
-  console.log(nameInputed);
+
+  var nameInputed = document.getElementById("addNameForm").value;
+  nameInputed = nameInputed + '\n';
+
+    nameArray.push(nameInputed);
+
+  console.log(nameArray);
+  document.getElementById("listArea").value = nameArray;
 }
 
 function sendList(){
+
+}
+
+function sendSymReq(){
+
+  var cbNone = document.getElementById("cbNone").value;
+  console.log(cbNone);
 
 }
