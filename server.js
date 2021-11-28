8af339d6e89d665e3238e48748095bd59a91369a
Reydel Gutierrez <>
2021-11-28 13:24:01 -0500

    Database mongoDB connected

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:3000';
 
 const app = express();
+const dbName = 'DB';
+const client = new MongoClient(url);
 
 const indexRoutes = require('./routes/index');
+
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+ client.close();
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
 
